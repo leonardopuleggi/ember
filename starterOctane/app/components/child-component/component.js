a7f1cdc17d9c488e512cbf57d27d8feb55708365
LeonardoP <leonardo.puleggi@gbst.com>
2020-10-28 11:46:31 +0000

    Ember 3.16.x (Octane) & 3.20.x

--- starterOctane/app/components/child-component/component.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class ChildComponentComponent extends Component {
-    get value() {
-        return 'test'
-    }
-}
--- starter_3_8_x/tests/integration/components/lazy-component/component-test.js
+++ starter_3_8_x/tests/integration/components/lazy-component/component-test.js
@@ -1,24 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | parent-component', function(hooks) {
+module('Integration | Component | lazy-component', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<ParentComponent />`);
+    await render(hbs`{{lazy-component}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <ParentComponent>
+      {{#lazy-component}}
         template block text
-      </ParentComponent>
+      {{/lazy-component}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
